6b853fe286c2c9e09c820c802aa0c05b3343e95b
Igor Moraes <igor.sgm@gmail.com>
2019-11-16 01:14:16 -0800

    Multiple fixes and improvements

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 
-Copyright (c) 2017 Piotr Krajewski <mits87@gmail.com>
+Copyright (c) 2019 Igor Moraes <igor.sgm@gmail.com>
 
 > Permission is hereby granted, free of charge, to any person obtaining a copy
 > of this software and associated documentation files (the "Software"), to deal
--- src/Traits/HasNestedAttributesTrait.php
+++ src/Traits/HasNestedAttributesTrait.php
@@ -1,19 +1,20 @@
 <?php
 
-namespace Eloquent\NestedAttributes\Traits;
+namespace Thomisticus\MappableModels\Traits;
 
-use Exception;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Support\Facades\DB;
+use Psr\Log\InvalidArgumentException;
 
 trait HasNestedAttributesTrait
 {
     /**
-     * Defined nested attributes 
+     * Defined nested attributes
      *
      * @var array
      */
@@ -40,7 +41,7 @@ trait HasNestedAttributesTrait
     /**
      * Fill the model with an array of attributes.
      *
-     * @param  array  $attributes
+     * @param array $attributes
      * @return $this
      *
      * @throws \Illuminate\Database\Eloquent\MassAssignmentException
@@ -55,14 +56,19 @@ trait HasNestedAttributesTrait
                     $this->acceptNestedAttributesFor[$attr] = $attributes[$attr];
                 }
             }
+
+            if (!empty($this->guarded) || empty($this->guarded) && empty($this->fillable)) {
+                $this->guarded = array_merge($this->guarded, $this->nested);
+            }
         }
+
         return parent::fill($attributes);
     }
-    
+
     /**
      * Save the model to the database.
      *
-     * @param  array  $options
+     * @param array $options
      * @return bool
      */
     public function save(array $options = [])
@@ -75,82 +81,174 @@ trait HasNestedAttributesTrait
 
         foreach ($this->getAcceptNestedAttributesFor() as $attribute => $stack) {
             $methodName = lcfirst(join(array_map('ucfirst', explode('_', $attribute))));
-    
+
             if (!method_exists($this, $methodName)) {
-                throw new Exception('The nested atribute relation "' . $methodName . '" does not exists.');
+                throw new InvalidArgumentException('The nested attribute relation "' . $methodName . '" does not exists.');
             }
 
             $relation = $this->$methodName();
-
-            if ($relation instanceof HasOne || $relation instanceof MorphOne) {
-                if (!$this->saveNestedAttributes($relation, $stack)) {
+            if ($relation instanceof BelongsTo) {
+                if (!$this->saveBelongToNestedAttributes($relation, $stack)) {
                     return false;
                 }
-            } else if ($relation instanceof HasMany || $relation instanceof MorphMany) {
-                foreach ($stack as $params) {
-                    if (!$this->saveManyNestedAttributes($this->$methodName(), $params)) {
+            } else {
+                if ($relation instanceof HasOne || $relation instanceof MorphOne) {
+                    if (!$this->saveOneNestedAttributes($relation, $stack)) {
                         return false;
                     }
+                } else {
+                    if ($relation instanceof HasMany || $relation instanceof MorphMany) {
+                        $modelRelation = $this->$methodName();
+                        $relatedKeyName = $modelRelation->getRelated()->getKeyName();
+                        $idsArray = array_map(function ($stack) use ($relatedKeyName) {
+                            return isset($stack[$relatedKeyName]) ? $stack[$relatedKeyName] : false;
+                        }, $stack);
+                        $idsNotDelete = array_filter($idsArray);
+
+                        //Syncing one-to-many relationships
+                        if (count($idsNotDelete) > 0) {
+                            $modelRelation->whereNotIn($relatedKeyName, $idsNotDelete)->delete();
+                        } else {
+                            $modelRelation->delete();
+                        }
+                        foreach ($stack as $params) {
+                            if (!$this->saveManyNestedAttributes($this->$methodName(), $params)) {
+                                return false;
+                            }
+                        }
+                    } else {
+                        if ($relation instanceof BelongsToMany) {
+                            $idsNesteds = [];
+                            foreach ($stack as $params) {
+                                $idsNesteds[] = $this->saveBelongsToManyNestedAttributes(
+                                    $this->$methodName()->getModel(),
+                                    $params
+                                );
+                            }
+
+                            $this->$methodName()->sync($idsNesteds);
+                        } else {
+                            throw new InvalidArgumentException('The nested attribute relation is not supported for "' . $methodName . '".');
+                        }
+                    }
                 }
-            } else {
-                throw new Exception('The nested atribute relation is not supported for "' . $methodName . '".');
             }
         }
 
         DB::commit();
+
         return true;
     }
 
     /**
      * Save the hasOne nested relation attributes to the database.
      *
-     * @param  Illuminate\Database\Eloquent\Relations  $relation
-     * @param  array                                   $params
+     * @param Illuminate\Database\Eloquent\Relations $relation
+     * @param array $params
      * @return bool
      */
-    protected function saveNestedAttributes($relation, array $params)
+    protected function saveBelongToNestedAttributes($relation, array $params)
     {
         if ($this->exists && $model = $relation->first()) {
             if ($this->allowDestroyNestedAttributes($params)) {
                 return $model->delete();
             }
-            return $model->update($stack);
-        } else if ($relation->create($stack)) {
+            return $model->update($params);
+        }
+
+        if ($related = $relation->create($params)) {
+            $belongs = $relation->getRelation();
+            $this->$belongs()->associate($related);
+            parent::save();
+
             return true;
         }
+
+        return false;
+    }
+
+    /**
+     * Save the hasOne nested relation attributes to the database.
+     *
+     * @param Illuminate\Database\Eloquent\Relations $relation
+     * @param array $params
+     * @return bool
+     */
+    protected function saveOneNestedAttributes($relation, array $params)
+    {
+        if ($this->exists && $model = $relation->first()) {
+            if ($this->allowDestroyNestedAttributes($params)) {
+                return $model->delete();
+            }
+            return $model->update($params);
+        }
+
+        if ($relation->create($params)) {
+            return true;
+        }
+
         return false;
     }
 
     /**
      * Save the hasMany nested relation attributes to the database.
      *
-     * @param  Illuminate\Database\Eloquent\Relations  $relation
-     * @param  array                                   $params
+     * @param Illuminate\Database\Eloquent\Relations $relation
+     * @param array $params
      * @return bool
      */
     protected function saveManyNestedAttributes($relation, array $params)
     {
-        if (isset($params['id']) && $this->exists) {
-            $model = $relation->findOrFail($params['id']);
+        $keyName = $relation->getModel()->getKeyName();
+
+        if (isset($params[$keyName]) && $this->exists) {
+            $model = $relation->findOrFail($params[$keyName]);
 
             if ($this->allowDestroyNestedAttributes($params)) {
                 return $model->delete();
-            }            
+            }
+
             return $model->update($params);
-        } else if ($relation->create($params)) {
+        }
+
+        if ($relation->create($params)) {
             return true;
         }
+
         return false;
+    }
+
+    /**
+     * Save the belongsToMany nested relation attributes to the database.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param array $params
+     * @return bool
+     */
+    protected function saveBelongsToManyNestedAttributes($model, array $params)
+    {
+        $keyName = $model->getKeyName();
+
+        $attributes = !empty($params[$keyName]) ? [$keyName => $params[$keyName]] : $params;
+
+        if ($model->nested) {
+            foreach ($model->nested as $nested) {
+                unset($attributes[$nested]);
+            }
+        }
+
+        $params = $model::updateOrCreate($attributes, $params);
+        return $params->$keyName;
     }
 
     /**
      * Check can we delete nested data
      *
-     * @param  array $params
+     * @param array $params
      * @return bool
      */
     protected function allowDestroyNestedAttributes(array $params)
     {
-        return isset($params[$this->destroyNestedKey]) && (bool) $params[$this->destroyNestedKey] == true;
+        return isset($params[$this->destroyNestedKey]) && (bool)$params[$this->destroyNestedKey] === true;
     }
 }
